f8fd5706e5fc17d1b9b51e19212e417d4cc852d8
giliard_j_carvalho <giliard_j_carvalho@10.1.48.180>
2013-09-24 14:26:55 -0300

    Criado o Porjeto e a classe principal InterfaceProjeto.

---
+++ InterfaceProjeto/src/br/com/senai/interfaceProjeto/principal/InterfaceProjeto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senai.interfaceProjeto.principal;
+
+/**
+ *
+ * @author giliard_j_carvalho
+ */
+public class InterfaceProjeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
